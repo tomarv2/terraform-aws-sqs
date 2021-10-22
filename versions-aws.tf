032b325b34259c0862ed8ad78ed9643f4ef92520
tomarv2 <11890959+tomarv2@users.noreply.github.com>
2021-10-22 09:12:40 -0700

    Initial commit

---
+++ versions-aws.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0.1"
+  required_providers {
+    aws = {
+      version = "~> 3.47"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.profile_to_use
+}
